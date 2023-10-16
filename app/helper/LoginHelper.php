9f56182f16f32941cb616b321d3ec1759f43ca5c
Alexis Ivanoff <alexis.ivanoff@hotmail.com>
2023-10-16 18:44:46 -0300

    Se agrego un 'LoginHelper' para el manejo de sesiones

---
+++ app/helper/LoginHelper.php
@@ -0,0 +1,35 @@
+<?php
+
+class LoginHelper {
+    public static function init() {
+        if (session_status() != PHP_SESSION_ACTIVE) {
+            session_start();
+        }
+    }
+
+    public static function login($user) {
+        LoginHelper::init();
+        $_SESSION['USER_ID'] = $user->id;
+        $_SESSION['USER_USER'] = $user->user; 
+    }
+
+    public static function logout() {
+        LoginHelper::init();
+        session_destroy();
+    }
+
+    public static function verify() {
+        LoginHelper::init();
+        if (!isset($_SESSION['USER_ID'])) {
+            header('Location: ' . BASE_URL . '/login');
+            die();
+        }
+    }
+
+
+
+
+
+
+
+}
